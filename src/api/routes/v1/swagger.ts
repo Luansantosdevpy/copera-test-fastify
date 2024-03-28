71f982306567c387e256e0e2a4a15e2bc7938119
luansantosdevpy <luansantosdev.py@gmail.com>
2024-03-28 14:22:33 -0300

    fix: change status code to delete in batch and complete in batch and resolve swagger route

--- src/api/routes/v1/swagger.ts
+++ src/api/routes/v1/swagger.ts
@@ -1,8 +1,8 @@
-import { FastifyInstance, FastifyReply } from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+
+import paths from '../../../swagger/paths';
 
 export default async (fastify: FastifyInstance): Promise<void> => {
-  await fastify.register(require('@fastify/swagger'));
-
   fastify.register(function corsPlugin(fastify, opts, done) {
     fastify.addHook('onRequest', (request, reply: FastifyReply, done) => {
       reply.header('Access-Control-Allow-Origin', '*');
@@ -16,13 +16,36 @@ export default async (fastify: FastifyInstance): Promise<void> => {
     done();
   });
 
+  fastify.addHook('onSend', async (request: FastifyRequest, reply: FastifyReply, payload: any) => {
+    if (request.routerPath === '/api/doc/json') {
+      reply.header('Access-Control-Allow-Origin', '*');
+      reply.header('Access-Control-Allow-Methods', 'GET');
+      reply.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+  });
+
+  await fastify.register(require('@fastify/swagger'), {
+    exposeRoute: true,
+    swagger: {
+      info: {
+        title: 'Api de controle de tarefas',
+        version: '1.0.0',
+      },
+      host: 'localhost:3500',
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json'],
+      paths,
+    },
+  });
+
   await fastify.register(require('@fastify/swagger-ui'), {
     routePrefix: '/doc',
     uiConfig: {
       docExpansion: 'full',
-      deepLinking: false
+      deepLinking: false,
     },
     staticCSP: true,
-    transformSpecificationClone: true
+    transformSpecificationClone: true,
   });
 };
