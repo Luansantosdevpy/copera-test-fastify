37be0da2ef7593901caf31f47c015076067473da
luansantosdevpy <luansantosdev.py@gmail.com>
2024-03-28 14:28:56 -0300

    fix: pattern routes

--- src/api/routes/v1/toDo.ts
+++ src/api/routes/v1/toDo.ts
@@ -6,13 +6,13 @@ import * as validations from '../../../application/validations/toDoSchema';
 export default async (fastify: FastifyInstance): Promise<void> => {
   const toDoController = container.resolve(ToDoController);
 
-  fastify.post('/create', { schema: validations.createSchema }, toDoController.create);
-  fastify.post('/complete-in-batch', { schema: validations.completeInBatchSchema }, toDoController.completeInBatch);
-  fastify.delete('/delete-in-batch', { schema: validations.deleteInBatchSchema }, toDoController.deleteInBatch);
-  fastify.get('/todo', toDoController.findAll);
-  fastify.get('/todo/get-count-todo', toDoController.getTodoCount);
-  fastify.get('/todo/:id', toDoController.findById);
-  fastify.put('/todo/update-description/:id', { schema: validations.updateDescriptionSchema }, toDoController.updateDescription);
-  fastify.put('/todo/update-status/:id', { schema: validations.updateStatusSchema }, toDoController.updateStatus);
-  fastify.delete('/todo/:id', { schema: validations.deleteTaskSchema }, toDoController.delete);
+  fastify.post('/task/create', { schema: validations.createSchema }, toDoController.create);
+  fastify.post('/task/complete-in-batch', { schema: validations.completeInBatchSchema }, toDoController.completeInBatch);
+  fastify.delete('/task/delete-in-batch', { schema: validations.deleteInBatchSchema }, toDoController.deleteInBatch);
+  fastify.get('/tasks', toDoController.findAll);
+  fastify.get('/task/get-count-todo', toDoController.getTodoCount);
+  fastify.get('/task/:id', toDoController.findById);
+  fastify.put('/task/update-description/:id', { schema: validations.updateDescriptionSchema }, toDoController.updateDescription);
+  fastify.put('/task/update-status/:id', { schema: validations.updateStatusSchema }, toDoController.updateStatus);
+  fastify.delete('/task/:id', { schema: validations.deleteTaskSchema }, toDoController.delete);
 };
