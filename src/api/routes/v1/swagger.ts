022ad011ff52d05501f82fe1e34e875a72e35093
luansantospravaler <francisco.machado@pravaler.com.br>
2024-03-27 22:27:44 -0300

    feat: [INCOMPLETE] create swagger routes

---
+++ src/api/routes/v1/swagger.ts
@@ -0,0 +1,25 @@
+import { FastifyInstance, FastifyReply } from 'fastify';
+
+export default async (fastify: FastifyInstance): Promise<void> => {
+  await fastify.register(require('@fastify/swagger'))
+
+  fastify.register(function corsPlugin(fastify, opts, done) {
+    fastify.addHook('onRequest', (request, reply: FastifyReply, done) => {
+      reply.header('Access-Control-Allow-Origin', '*')
+      reply.header('Access-Control-Allow-Methods', 'POST, GET, PUT, OPTIONS, PATCH, DELETE')
+      reply.header('Access-Control-Expose-Headers', 'X-file-name')
+      done()
+    });
+    done()
+  });
+
+  await fastify.register(require('@fastify/swagger-ui'), {
+    routePrefix: '/doc',
+    uiConfig: {
+        docExpansion: 'full',
+        deepLinking: false
+    },
+    staticCSP: true,
+    transformSpecificationClone: true
+  })
+};
